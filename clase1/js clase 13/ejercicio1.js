2b6d51f5904b3acde1c206437b33953d2cdc7b1c
Alan Guerrero García <age_allison@hotmail.com>
2022-04-25 21:55:45 -0500

    añade ejercicio 1 de clase 13

---
+++ clase1/js clase 13/ejercicio1.js
@@ -0,0 +1,126 @@
+const mentorsArray = [
+    {
+        name:"Ivan Lopez Banda",
+        scores:[
+            {
+                signature:"HTML",
+                score:8
+            },
+            {
+                signature:"CSS",
+                score:10
+            },
+            {
+                signature:"JS",
+                score:8
+            },
+            {
+                signature:"ReactJS",
+                score:8
+            }
+        ]
+    },
+    {
+        name:"Alejandra Guzman Olvera",
+        scores:[
+            {
+                signature:"HTML",
+                score:8
+            },
+            {
+                signature:"CSS",
+                score:7
+            },
+            {
+                signature:"JS",
+                score:10
+            },
+            {
+                signature:"ReactJS",
+                score:10
+            }
+        ]
+    },
+    {
+        name:"Alan Guerrero García",
+        scores:[
+            {
+                signature:"HTML",
+                score:9
+            },
+            {
+                signature:"CSS",
+                score:9
+            },
+            {
+                signature:"JS",
+                score:10
+            },
+            {
+                signature:"ReactJS",
+                score:9
+            }
+        ]
+    },
+    {
+        name:"Oscar Diaz Alarcon",
+        scores:[
+            {
+                signature:"HTML",
+                score:8
+            },
+            {
+                signature:"CSS",
+                score:10
+            },
+            {
+                signature:"JS",
+                score:9
+            },
+            {
+                signature:"ReactJS",
+                score:10
+            }
+        ]
+    },
+    {
+        name:"Pedro Diaz Alarcon",
+        scores:[
+            {
+                signature:"HTML",
+                score:8
+            },
+            {
+                signature:"CSS",
+                score:10
+            },
+            {
+                signature:"JS",
+                score:9
+            },
+            {
+                signature:"ReactJS",
+                score:10
+            }
+        ]
+    }
+]
+
+
+//Empieza el ejercicio 1//
+const vowels = /[aeiou]/gi
+
+
+
+
+let mentorsVowel = mentorsArray.filter((mentor)=>{
+    if (mentor.name[0].match(vowels)){
+        return mentor.name
+    }
+    let letras = []
+    mentor.name.split(" ")
+})
+console.log(mentorsVowel)
+
+
+
